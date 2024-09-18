6cdef45ad534d311e45e7aa2753bdaef1e1dbbee
unknown <mshlandyk2912@gmail.com>
2024-09-18 19:19:21 +0300

    fix: fix header component

--- veps-site/src/app/app-routing.module.ts
+++ veps-site/src/app/app-routing.module.ts
@@ -4,9 +4,6 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { VacanciesPageComponent } from './landing-page/vacancies-page/vacancies-page.component';
 
 export const routes: Routes = [
-  // { path: 'home', component: HomePageComponent },
-  // { path: 'about', component: AboutPageComponent },
-  // { path: 'services', component: ServicesPageComponent },
   { path: 'vacancies', component: VacanciesPageComponent },
   { path: '', component: LandingPageComponent },
   { path: '**', redirectTo: '' }
